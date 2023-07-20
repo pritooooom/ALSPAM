4dd7d88d8e4905b3ccf130b33c1d65f98ff51001
YASH <129831825+PRADHAN474@users.noreply.github.com>
2023-07-20 11:46:28 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -8,8 +8,8 @@ START_BUTTON = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
     ],
     [
-        Button.url("• ᴅᴇᴠᴇʟᴏᴘᴇʀ 😎", "https://t.me/@PRADHAN474"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ 🚀", "https://t.me/BWANDARLOK")
+        Button.url("🥀 ᴅᴇᴠᴇʟᴏᴘᴇʀ 🥀", "https://t.me/@PRADHAN474"),
+        Button.url("✨ sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/BWANDARLOK")
     ],
     [
         Button.url("• ʀᴇᴘᴏ •", "https://t.me/PRADHAN474")
@@ -34,7 +34,7 @@ async def start(event):
         bot_name = AltBot.first_name
         bot_id = AltBot.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [🦋⃟ ᴠͥɪͣᴘͫ 🇴 🇽 𝐘 𝐆 𝐄 𝐍⃝⃝⃪⃕🥀**\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : 🦋⃟ ᴠͥɪͣᴘͫ 🇴 🇽 𝐘 𝐆 𝐄 𝐍⃝⃝⃪⃕🥀**\n\n"
         TEXT += f"» **xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
